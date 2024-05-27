746df8069fa7aecc3ec7d4f98951cbf00553b783
Akhileshkazam <akhilesh@kazam.in>
2024-05-27 12:31:53 +0530

    changes

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,8 +1,15 @@
 const { test } = require("@playwright/test");
 
-test("Login Flow", async ({ browser }) => {
+("Login Flow", async ({ browser }) => {
   //chrome - plugins / cookies;
-  const context = await browser.newContext();
   const page = await context.newPage();
+  const email = page.locator('#large-input');
   await page.goto("https://novo.kazam.in/");
+  console.log(await page.title());
+  //login credential
+  await email.type("akhilesh@kazam.in");
+  await page.locator("#password").fill("Akbl@1724");
+  await page.getByRole("button",{name:"login"}).click();
+  await page.goto("https://novo.kazam.in/org");
+ 
 });
